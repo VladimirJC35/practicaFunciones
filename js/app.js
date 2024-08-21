9a14c3ec99e6857d44b2dc428a63e856cac8a176
VladimirJC35 <vladimirjc25@gmail.com>
2024-08-20 21:08:57 -0500

    funcion Multiplicar

--- js/app.js
+++ js/app.js
@@ -28,13 +28,16 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// Write your code here
+// Write your code here 'The product of 5 and 9 is 45.'
 function multiply(a, b) { //eslint-disable-line
-
+    let multiplicar= a*b;
+    let mensaje = "The product of "+a+ " and " +b+ " is " +multiplicar+ ".";
+    return [multiplicar, mensaje];
+   
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
